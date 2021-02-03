faaf0f06698c13f91ce38dce59853f4624661669
oof888 <76743513+oof888@users.noreply.github.com>
2021-02-03 12:16:37 -0500

    Update JustinScript.js

--- JustinScript.js
+++ JustinScript.js
@@ -1,5 +1,5 @@
 function showMessage(){
-document.getElementById("messageSecret").innerHTML="Hello World!"; 
+document.getElementById("messageSecret").innerHTML="https://docs.google.com/forms/d/e/1FAIpQLScPd02R7ru5zixQWUB2ErmPz4skefoeXZr7nO3ww2b5L8Fhrg/viewform?usp=sf_link"; 
 }
 
  var clicks=0;
